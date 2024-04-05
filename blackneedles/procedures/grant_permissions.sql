ba1f574924a9a2ec5a22d7bec60763d54c9226e9
Thiago F Pappacena <pappacena@gmail.com>
2024-04-05 12:34:26 -0300

    Adding service logs following commmand

--- blackneedles/procedures/grant_permissions.sql
+++ blackneedles/procedures/grant_permissions.sql
@@ -2,4 +2,4 @@ GRANT USAGE ON PROCEDURE __blackneedles__.list_services (string) TO {grant_targe
 GRANT USAGE ON PROCEDURE __blackneedles__.check_status (varchar) TO {grant_target};
 GRANT USAGE ON PROCEDURE __blackneedles__.describe_service (varchar) TO {grant_target};
 GRANT USAGE ON PROCEDURE __blackneedles__.alter_service (varchar, varchar) TO {grant_target};
-GRANT USAGE ON PROCEDURE __blackneedles__.list_compute_pool () TO {grant_target};
+GRANT USAGE ON PROCEDURE __blackneedles__.get_service_logs (varchar, varchar, varchar) TO {grant_target};
